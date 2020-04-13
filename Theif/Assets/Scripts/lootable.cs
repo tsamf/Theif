8a7c27072898264328a629702ee9bb0b97a2e492
samfordt <samfordt@gmail.com>
2020-04-13 17:54:34 -0500

    Added some sneaky music.

--- Theif/Assets/Scripts/lootable.cs
+++ Theif/Assets/Scripts/lootable.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class lootable : MonoBehaviour
 {
+    public float weight = 1.0f;
+    public float value = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
